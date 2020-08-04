4cc90d08702ecc27f4cf498bcf8688c12007374e
powermiso <powermiso@naver.com>
2020-08-04 16:11:35 +0900

    파일 병합 및 충돌 해결

---
+++ src/main/java/com/ssafy/edu/model/mapper/TourMapper.java
@@ -0,0 +1,101 @@
+
+package com.ssafy.edu.model.mapper;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.ssafy.edu.model.vo.AreaVo;
+import com.ssafy.edu.model.vo.ContentTypeVo;
+import com.ssafy.edu.model.vo.DetailCourse;
+import com.ssafy.edu.model.vo.DetailCultural;
+import com.ssafy.edu.model.vo.DetailFestival;
+import com.ssafy.edu.model.vo.DetailLeports;
+import com.ssafy.edu.model.vo.DetailLodgment;
+import com.ssafy.edu.model.vo.DetailRestaurant;
+import com.ssafy.edu.model.vo.DetailShopping;
+import com.ssafy.edu.model.vo.DetailTourPlace;
+import com.ssafy.edu.model.vo.DetailVo;
+import com.ssafy.edu.model.vo.SigunguVo;
+import com.ssafy.edu.model.vo.TourInfoVo;
+
+
+public interface TourMapper {
+	//전체 관광정보 반환(범위?)
+	public List<TourInfoVo> getTourInfoList();
+	//관광정보 입력
+	//전체 관광정보 반환(범위?)
+	//전체 관광정보 반환(범위?)
+		//현재위치, 반경에 따른 관광정보 반환+정렬
+		List<TourInfoVo> getTourInfoByHere(@Param("mapx") double mapx,@Param("mapy") double mapy,@Param("distance") double distance);
+//		//현재위치, 반경에 따른 관광정보 반환(이용가능)+정렬
+//		List<TourInfoVo> getAvailableTourInfoByHere(int mapx,int mapy,int distance);
+//		//현재위치, 반경에 따른 관광정보 반환(이용 불가능)+정렬
+//		List<TourInfoVo> getUnAvailableTourInfoByHere(int mapx,int mapy,int distance);
+		//지역에 따른 관광정보 반환
+		List<TourInfoVo> getTourInfoByLocation(@Param("areaCode") int areaCode,@Param("sigunguCode")int sigunguCode);
+		//Id로? 상세정보 반환
+		DetailCourse getDetailCourseByCId(@Param("contentId") int contentId);
+		//Id로? 상세정보 반환
+		DetailCultural getDetailCulturalByCId(@Param("contentId") int contentId);
+		//Id로? 상세정보 반환
+		DetailFestival getDetailFestivalByCId(@Param("contentId") int contentId);
+		//Id로? 상세정보 반환
+		DetailLeports getDetailLeportsByCId(@Param("contentId") int contentId);
+		//Id로? 상세정보 반환
+		DetailLodgment getDetailLodgmentByCId(@Param("contentId") int contentId);
+		//Id로? 상세정보 반환
+		DetailRestaurant getDetailRestaurantByCId(@Param("contentId") int contentId);
+		//Id로? 상세정보 반환
+		DetailShopping getDetailShoppingByCId(@Param("contentId") int contentId);
+		//Id로? 상세정보 반환
+		DetailTourPlace getDetailTourPlaceByCId(@Param("contentId") int contentId);
+		//관광정보 입력
+		public boolean insertDetailCourse(HashMap<String,Object> parameters);
+		boolean insertDetailCultural(HashMap<String,Object> parameters);
+		//관광정보 입력
+		boolean insertDetailFestival(HashMap<String,Object> parameters);
+		//관광정보 입력
+		boolean insertDetailLeports(HashMap<String,Object> parameters);
+		//관광정보 입력
+		boolean insertDetailLodgment(HashMap<String,Object> parameters);
+		//관광정보 입력
+		boolean insertDetailRestaurant(HashMap<String,Object> parameters);
+		//관광정보 입력
+		boolean insertDetailShopping(HashMap<String,Object> parameters);
+		//관광정보 입력
+		boolean insertDetailTourPlace(HashMap<String,Object> parameters);
+		//관광정보 수정
+		boolean updateDetailCourse(HashMap<String,Object> parameters);
+		//관광정보 수정
+		boolean updateDetailCultural(HashMap<String,Object> parameters);
+		//관광정보 수정
+		boolean updateDetailFestival(HashMap<String,Object> parameters);
+		//관광정보 수정
+		boolean updateDetailLeports(HashMap<String,Object> parameters);
+		//관광정보 수정
+		boolean updateDetailLodgment(HashMap<String,Object> parameters);
+		//관광정보 수정
+		boolean updateDetailRestaurant(HashMap<String,Object> parameters);
+		//관광정보 수정
+		boolean updateDetailShopping(HashMap<String,Object> parameters);
+		//관광정보 수정
+		boolean updateDetailTourPlace(HashMap<String,Object> parameters);
+		
+		//관광정보 삭제
+		boolean deleteTourInfo(@Param("contentId") int contentId);
+//		//상세정보 입력
+//		boolean insertDetail(DetailVo dt);
+//		//상세정보 수정
+//		boolean updateDetail(DetailVo dt);
+//		//상세정보 삭제
+//		boolean deleteDetail(int contentId);
+		//지역 정보 반환
+		List<AreaVo> getArea();//시,도
+		List<SigunguVo> getSigungu(int areaCode);//시,군,구
+		//컨텐츠 타입 반환
+		List<ContentTypeVo> getContentType();
+
+
+}
---
+++ src/main/java/com/ssafy/edu/model/service/TourServiceImpl.java
@@ -0,0 +1,205 @@
+
+package com.ssafy.edu.model.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.ssafy.edu.model.dao.ITourDAO;
+import com.ssafy.edu.model.vo.AreaVo;
+import com.ssafy.edu.model.vo.ContentTypeVo;
+import com.ssafy.edu.model.vo.DetailCourse;
+import com.ssafy.edu.model.vo.DetailCultural;
+import com.ssafy.edu.model.vo.DetailFestival;
+import com.ssafy.edu.model.vo.DetailLeports;
+import com.ssafy.edu.model.vo.DetailLodgment;
+import com.ssafy.edu.model.vo.DetailRestaurant;
+import com.ssafy.edu.model.vo.DetailShopping;
+import com.ssafy.edu.model.vo.DetailTourPlace;
+import com.ssafy.edu.model.vo.SigunguVo;
+import com.ssafy.edu.model.vo.TourInfoVo;
+
+@Component
+public class TourServiceImpl implements ITourService{
+	@Autowired
+	private ITourDAO dao;
+
+	public void setDao(ITourDAO dao) {
+		this.dao = dao;
+	}
+
+	@Override
+	public List<TourInfoVo> getTourInfoList() {
+		return dao.getTourInfoList();
+	}
+
+	@Override
+	public boolean insertDetailCourse(TourInfoVo ti, DetailCourse d) {
+		return dao.insertDetailCourse(ti, d);
+	}
+
+	@Override
+	public List<TourInfoVo> getTourInfoByHere(double mapx, double mapy, double distance) {
+		return dao.getTourInfoByHere(mapx, mapy, distance);
+	}
+
+	@Override
+	public List<TourInfoVo> getAvailableTourInfoByHere(int mapx, int mapy, int distance) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<TourInfoVo> getUnAvailableTourInfoByHere(int mapx, int mapy, int distance) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<TourInfoVo> getTourInfoByLocation(int areaCode, int sigunguCode) {
+		return dao.getTourInfoByLocation(areaCode, sigunguCode);
+	}
+
+
+	@Override
+	public DetailCourse getDetailCourseByCId(int contentId) {
+		return dao.getDetailCourseByCId(contentId);
+	}
+
+	@Override
+	public DetailCultural getDetailCulturalByCId(int contentId) {
+		// TODO Auto-generated method stub
+		return dao.getDetailCulturalByCId(contentId);
+	}
+
+	@Override
+	public DetailFestival getDetailFestivalByCId(int contentId) {
+		return dao.getDetailFestivalByCId(contentId);
+	}
+
+	@Override
+	public DetailLeports getDetailLeportsByCId(int contentId) {
+		return dao.getDetailLeportsByCId(contentId);
+	}
+
+	@Override
+	public DetailLodgment getDetailLodgmentByCId(int contentId) {
+		return dao.getDetailLodgmentByCId(contentId);
+	}
+
+	@Override
+	public DetailRestaurant getDetailRestaurantByCId(int contentId) {
+		return dao.getDetailRestaurantByCId(contentId);
+	}
+
+	@Override
+	public DetailShopping getDetailShoppingByCId(int contentId) {
+		return dao.getDetailShoppingByCId(contentId);
+	}
+
+	@Override
+	public DetailTourPlace getDetailTourPlaceByCId(int contentId) {
+		return dao.getDetailTourPlaceByCId(contentId);
+	}
+
+	@Override
+	public boolean insertDetailCultural(TourInfoVo ti, DetailCultural d) {
+		return dao.insertDetailCultural(ti, d);
+	}
+
+	@Override
+	public boolean insertDetailFestival(TourInfoVo ti, DetailFestival d) {
+		return dao.insertDetailFestival(ti, d);
+	}
+
+	@Override
+	public boolean insertDetailLeports(TourInfoVo ti, DetailLeports d) {
+		return dao.insertDetailLeports(ti, d);
+	}
+
+	@Override
+	public boolean insertDetailLodgment(TourInfoVo ti, DetailLodgment d) {
+		return dao.insertDetailLodgment(ti, d);
+	}
+
+	@Override
+	public boolean insertDetailRestaurant(TourInfoVo ti, DetailRestaurant d) {
+		return dao.insertDetailRestaurant(ti, d);
+	}
+
+	@Override
+	public boolean insertDetailShopping(TourInfoVo ti, DetailShopping d) {
+		return dao.insertDetailShopping(ti, d);
+	}
+
+	@Override
+	public boolean insertDetailTourPlace(TourInfoVo ti, DetailTourPlace d) {
+		return dao.insertDetailTourPlace(ti, d);
+	}
+
+	@Override
+	public boolean updateDetailCourse(TourInfoVo ti, DetailCourse d) {
+		return dao.updateDetailCourse(ti, d);
+	}
+
+	@Override
+	public boolean updateDetailCultural(TourInfoVo ti, DetailCultural d) {
+		return dao.updateDetailCultural(ti, d);
+	}
+
+	@Override
+	public boolean updateDetailFestival(TourInfoVo ti, DetailFestival d) {
+		return dao.updateDetailFestival(ti, d);
+	}
+
+	@Override
+	public boolean updateDetailLeports(TourInfoVo ti, DetailLeports d) {
+		return dao.updateDetailLeports(ti, d);
+	}
+
+	@Override
+	public boolean updateDetailLodgment(TourInfoVo ti, DetailLodgment d) {
+		return dao.updateDetailLodgment(ti, d);
+	}
+
+	@Override
+	public boolean updateDetailRestaurant(TourInfoVo ti, DetailRestaurant d) {
+		return dao.updateDetailRestaurant(ti, d);
+	}
+
+	@Override
+	public boolean updateDetailShopping(TourInfoVo ti, DetailShopping d) {
+		return dao.updateDetailShopping(ti, d);
+	}
+
+	@Override
+	public boolean updateDetailTourPlace(TourInfoVo ti, DetailTourPlace d) {
+		return dao.updateDetailTourPlace(ti, d);
+	}
+
+	@Override
+	public boolean deleteTourInfo(int contentId) {
+		return dao.deleteTourInfo(contentId);
+	}
+
+	@Override
+	public List<AreaVo> getArea() {
+		return dao.getArea();
+	}
+
+	@Override
+	public List<SigunguVo> getSigungu(int areaCode) {
+		return dao.getSigungu(areaCode);
+	}
+
+	@Override
+	public List<ContentTypeVo> getContentType() {
+		return dao.getContentType();
+	}
+
+	
+
+
+
+}
